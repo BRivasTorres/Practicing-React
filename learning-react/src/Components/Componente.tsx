61d4e04e953d668265fb008fb7f57b0f9b481d0b
Billy Torres <btorresr75@gmail.com>
2023-04-18 17:37:26 -0600

    components

---
+++ learning-react/src/Components/Componente.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Componente extends Component {
+    render() {
+        return <h1>Hello im a Component</h1>
+    }
+}
+
+export default Componente
