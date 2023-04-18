b5c53cd8b6c722f7eca3cab2e2d99ce640c144a0
Billy Torres <btorresr75@gmail.com>
2023-04-18 17:45:53 -0600

    components

--- learning-react/src/Components/Componente.tsx
+++ learning-react/src/Components/Componente.tsx
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
 
-class Componente extends Component {
-    render() {
-        return <h1>Hello im a Component</h1>
-    }
+//&Antigua manera en como eran creado los componentes, esta manera se hacia por medio de clases.
+// class Componente extends Component {
+//     render() {
+//         return <h1>Hello im a Component</h1>
+//     }
+// }
+
+//&Esta es la manera actual de crear un componente, una manera mas clara y legible.
+const Componente = () => {
+    return <h2>Hello im a Component🙂</h2>
 }
 
 export default Componente
