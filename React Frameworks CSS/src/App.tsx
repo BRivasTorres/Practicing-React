4c8f7fcfc1f8ac9ae46a7ad5f8d05821f4e93c91
Billy Torres <btorresr75@gmail.com>
2023-04-26 14:52:31 -0600

    reactBootstrap

--- React Frameworks CSS/src/App.tsx
+++ React Frameworks CSS/src/App.tsx
@@ -1,11 +1,13 @@
 import Bootstrap from './components/Bootstrap'
+import ReactBootstrap from './components/ReactBootstrap'
 
 function App() {
 
   return (
     <>
       <div>Frameworks CSS con React</div>
-      <Bootstrap />
+      {/* <Bootstrap /> */}
+      <ReactBootstrap />
     </>
   )
 }
