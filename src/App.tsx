76cce3a249a3faae19cb0439ffcc802ccd62672f
Billy Torres <btorresr75@gmail.com>
2023-04-30 11:04:36 -0600

    counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import Counter from "./components/Counter"
+
 function App() {
 
   return (
     <>
       <h2 className="text-blue-600 text-[3rem]">React + TS</h2>
+      <Counter />
     </>
   )
 }
---
+++ src/components/Counter.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Counter = () => {
+    return (
+        <div className='text-yellow-400'>
+            <h3>Counter: </h3>
+        </div>
+    )
+}
+
+export default Counter
