d3b0de19d6652bd5e69a54e964d4f2b4e4a6601f
Billy Torres <btorresr75@gmail.com>
2023-04-30 10:51:45 -0600

    counter

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ function App() {
 
   return (
     <>
-      <h2 className="text-blue-600 text-[6rem]">React + TS</h2>
+      <h2 className="text-blue-600 text-[3rem]">React + TS</h2>
     </>
   )
 }
