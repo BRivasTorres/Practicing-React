3b13ab8f502e8dfc99345f2aa65d6f823d203804
Billy Torres <btorresr75@gmail.com>
2023-05-20 15:31:41 -0600

    validacionesDeFormularios

--- Formularios/src/components/ContactForm.tsx
+++ Formularios/src/components/ContactForm.tsx
@@ -10,9 +10,31 @@ const initialForm = {
 
 const validationsForm = (form) => {
     const errors = {}
+    const regexName = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/;
+    const regexEmail = /^(\w+[/./-]?){1,}@[a-z]+[/.]\w{2,}$/;
+    const regexComments = /^.{1,255}$/;
+
 
     if (!form.name.trim()) {
         errors.name = "El campo ´Nombre´ es requerido"
+    } else if (!regexName.test(form.name.trim())) {
+        errors.name = "El campo Nombre solo acepta caracteres Alfabeticos"
+    }
+
+    if (!form.email.trim()) {
+        errors.email = "El campo Email es requerido"
+    } else if (!regexEmail.test(form.email.trim())) {
+        errors.email = "El campol Email debe ser un correo"
+    }
+
+    if (!form.subject.trim()) {
+        errors.subject = "El campo Subject es requerido"
+    }
+
+    if (!form.comments.trim()) {
+        errors.comments = "El campo Comments es requerido"
+    } else if (!regexComments.test(form.comments.trim())) {
+        errors.comments = "El campo Comments deve contener solamente 255 caracteres"
     }
 
     return errors
