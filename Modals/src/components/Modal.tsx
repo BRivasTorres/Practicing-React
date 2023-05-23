b4624281b8e4c7ba84db6ef3c8e6e3c1d41d9374
Billy Torres <btorresr75@gmail.com>
2023-05-22 18:54:06 -0600

    Project Modals Finalized

--- Modals/src/components/Modal.tsx
+++ Modals/src/components/Modal.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
-const Modal = ({ children }) => {
+const Modal = ({ children, isOpen, closeModal }) => {
+    const handleModalContainerClikc = e => e.stopPropagation()
     return (
-        <div className='is-open fixed z-[999] top-0 left-0 w-[100%] min-h-screen modal-bg justify-center items-center border hidden'>
-            <div className='modal-container relative bg-white p-[1rem] '>
-                <button className='modal-close absolute top-[1rem] right-[1rem]'>X</button>
+        <div onClick={closeModal} className={`${isOpen && "is-open "}
+            fixed z-[999] top-0 left-0 w-[100%] min-h-screen modal-bg justify-center items-center border hidden`}>
+            <div className='modal-container relative bg-white p-[1rem]' onClick={handleModalContainerClikc}>
+                <button className='modal-close absolute top-[1rem] right-[1rem] bg-gray-200 w-4 rounded' onClick={closeModal}>X</button>
                 {children}
             </div>
         </div>
--- Modals/src/components/Modals.tsx
+++ Modals/src/components/Modals.tsx
@@ -8,7 +8,15 @@ const Modals = () => {
 
     return (
         <>
-            <Modal>
+            <h2 className='text-[2rem] font-bold mx-4'>Modales</h2>
+            <button className='bg-gray-300 w-[5rem] p-[.3rem] font-bold rounded-[10px] mx-4' onClick={openModal1}>Modal 1</button>
+            <Modal isOpen={isOpenModal1} closeModal={closeModal1} >
+                <h2>Modal 1</h2>
+                <p>Contenido del modal 1</p>
+                <img src="https://placeimg.com/300/300/nature" alt="nature" />
+            </Modal>
+            <button className='bg-gray-300 w-[5rem] p-[.3rem] font-bold rounded-[10px]' onClick={openModal2}>Modal 2</button>
+            <Modal isOpen={isOpenModal2} closeModal={closeModal2}>
                 <h2>Modal 1</h2>
                 <p>Contenido del modal 1</p>
                 <img src="https://placeimg.com/300/300/nature" alt="nature" />
