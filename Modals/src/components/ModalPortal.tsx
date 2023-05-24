1e34813689808292c58c42e22c416f10ec272afd
Billy Torres <btorresr75@gmail.com>
2023-05-24 17:38:42 -0600

    Portals in React

---
+++ Modals/src/components/ModalPortal.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react'
+
+const ModalPortal = ({ children, isOpen, closeModal }) => {
+    const handleModalContainerClikc = e => e.stopPropagation()
+    return ReactDOM.createPortal(
+        <div onClick={closeModal} className={`${isOpen && "is-open "}
+            fixed z-[999] top-0 left-0 w-[100%] min-h-screen modal-bg justify-center items-center border hidden`}>
+            <div className='modal-container relative bg-white p-[1rem]' onClick={handleModalContainerClikc}>
+                <button className='modal-close absolute top-[1rem] right-[1rem] bg-gray-200 w-4 rounded' onClick={closeModal}>X</button>
+                {children}
+            </div>
+        </div>,
+        document.getElementById("modal")
+    )
+}
+
+export default ModalPortal
--- Modals/src/components/Modals.tsx
+++ Modals/src/components/Modals.tsx
@@ -5,6 +5,7 @@ import { useModal } from '../Hooks/useModal'
 const Modals = () => {
     const [isOpenModal1, openModal1, closeModal1] = useModal(false)
     const [isOpenModal2, openModal2, closeModal2] = useModal(false)
+    const [isOpenPortal, openModalPortal, closeModalportal] = useModal(false)
 
     return (
         <>
@@ -20,6 +21,12 @@ const Modals = () => {
                 <h2>Modal 1</h2>
                 <p>Contenido del modal 1</p>
                 <img src="https://placeimg.com/300/300/nature" alt="nature" />
+            </Modal>
+            <button className='bg-gray-300 w-[5rem] p-[.3rem] font-bold rounded-[10px]' onClick={openModalPortal}>Modal 2</button>
+            <Modal isOpen={isOpenPortal} closeModal={closeModalportal}>
+                <h2>Modal Portal</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet ipsam pariatur maiores doloribus enim. Similique ipsam est maxime amet in nulla quo quasi qui, non, ipsa nisi corrupti rem laborum. </p>
+                <img src="https://placeimg.com/300/300/tech" alt="tech" />
             </Modal>
         </>
     )
