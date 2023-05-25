df872e5b0bdb9944b89ecf71083d1478a8161615
Billy Torres <btorresr75@gmail.com>
2023-05-25 10:46:08 -0600

    Enrutado en una sola linea

--- React Router/ReactRouter--V5/src/App.jsx
+++ React Router/ReactRouter--V5/src/App.jsx
@@ -1,5 +1,7 @@
 import BasicConcepts from "./components/BasicConcepts"
 import {BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import Acerca from "./pages/Acerca"
+import Contacto from "./pages/Contacto"
 
 function App() {
   return (
@@ -10,12 +12,8 @@ function App() {
             <Route exact path="/">
               <h3>Home</h3>
             </Route>
-            <Route exact path="/acerca">
-              <h3>Acerca</h3>
-            </Route>
-            <Route exact path="/contacto">
-              <h3>Contacto</h3>
-            </Route>
+            <Route exact path="/acerca" component={Acerca} />
+            <Route exact  path="/contacto" component={Contacto} />
         </Switch>
       </Router>
     </>
