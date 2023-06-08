89ab5551e85b0a11e7caeb6a9f03e0bb16a12698
Billy Torres <btorresr75@gmail.com>
2023-06-08 17:36:46 -0600

    Memorización de valores calculados con useMemo

--- React General2/src/components/CounterChild.jsx
+++ React General2/src/components/CounterChild.jsx
@@ -1,6 +1,14 @@
-import { memo } from "react"
+import { useMemo,memo } from "react"
 
 const CounterChild = (props) => {
+  const num = useMemo(() => {
+    let num1 = 0
+    for(let i = 0; i < 100000000; i++) {
+      num1++
+    }
+    return num1
+  }, [])
+  
     console.log("renderized")
   return (
     <div style={{border:"thin solid white", margin:"1rem", padding:"1rem"}}>
@@ -8,6 +16,7 @@ const CounterChild = (props) => {
       <h3>{props.counter}</h3> 
       <button onClick={props.handleSum}>+</button>
       <button onClick={props.handleRest}>-</button>
+      <h3>{num}</h3>
     </div>
   )
 }
