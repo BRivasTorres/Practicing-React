ad66a8083b1c41b0f993818fe395c97bc2afac6e
Billy Torres <btorresr75@gmail.com>
2023-08-01 16:23:10 -0600

    Multi idioma con React

--- React Context/src/components/Header.jsx
+++ React Context/src/components/Header.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-const Header = ({theme, handleTheme}) => {
+const Header = ({theme, handleTheme, texts, handleLanguage}) => {
   return (
     <div  className={theme } >
       <header>
-        <h2>Mi aplicación SIN Context API</h2>
+        <h2>{texts.headerTitle}</h2>
         <h3>Mi Cabecera</h3>
-        <select name="languaje" >
+        <select name="languaje" onChange={handleLanguage}>
           <option value="es">ES</option>
           <option value="en">EN</option>
         </select>
@@ -14,7 +14,7 @@ const Header = ({theme, handleTheme}) => {
         <label htmlFor="light">Claro</label>
         <input type='radio' name='theme' id='dark' value="dark"  onClick={handleTheme}/>
         <label htmlFor="light">Oscuro</label>
-        <button>Iniciar Seción</button>
+        <button>{texts.buttonLogin} </button>
       </header>
     </div>
   )
--- React Context/src/components/MyPage.jsx
+++ React Context/src/components/MyPage.jsx
@@ -4,9 +4,23 @@ import Header from "./Header"
 import Main from "./Main"
 
 const initialTheme = "light"
+const initialLanguaje = "en"
+
+const translations = {
+  es: {
+    headerTitle : "Mi aplicacion SIN CONTEXT Api",
+    buttonLogin : "Iniciar sesión"
+  },
+  en: {
+    headerTitle : "My application without Context",
+    buttonLogin : "Login"
+  }
+}
 
 const MyPage = () => {
   const [theme, setTheme] = useState(initialTheme)
+  const [language, setLanguage] = useState(initialLanguaje)
+  const [texts, setTexts] = useState(translations[language])
   
   const handleTheme = (e) => {
     if(e.target.value === "light") {
@@ -16,11 +30,22 @@ const MyPage = () => {
     }
   }
   
+  const handleLanguaje = (e) => {
+    if(e.target.value === "es") {
+      setLanguage("es")
+      setTexts(translations.es)
+    } else {
+      setLanguage("en")
+      setTexts(translations.en)
+    }
+    console.log(e.target.value)
+  }
+  
   return (
     <div>
-        <Header theme={theme} handleTheme={handleTheme} />
-        <Main theme={theme}/>
-        <Footer theme={theme}/>
+        <Header theme={theme} handleTheme={handleTheme} texts={texts} handleLanguaje={handleLanguaje} />
+        <Main theme={theme} texts={texts}/>
+        <Footer theme={theme} texts={texts}/>
     </div>
   )
 }
